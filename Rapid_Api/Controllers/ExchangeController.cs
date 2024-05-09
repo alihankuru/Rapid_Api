7fc7acb329103d3cc18de3efbab23b797969d54c
AK <ali.kuru6@hotmail.com>
2024-05-09 21:53:53 +0300

    Add project files.

---
+++ Rapid_Api/Controllers/ExchangeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Rapid_Api.Models;
+
+namespace Rapid_Api.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8e7639e9d7msh014c646e6aaa9acp15e24bjsn5fe00f032feb" },
+        { "X-RapidAPI-Host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values);
+            }
+
+        
+        }
+    }
+}
---
+++ Rapid_Api/Controllers/ImdbSeriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Rapid_Api.Models;
+
+namespace Rapid_Api.Controllers
+{
+    public class ImdbSeriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8e7639e9d7msh014c646e6aaa9acp15e24bjsn5fe00f032feb" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ImdbSeriesViewModel>>(body);
+                return View(values);
+            }
+          
+        }
+    }
+}
